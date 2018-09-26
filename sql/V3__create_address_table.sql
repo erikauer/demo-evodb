734b50165c31e8810ce83c7724d80548a2ce798d
erikauer <erikauer@gmx.at>
2018-09-26 19:23:49 +0200

    add new migration

---
+++ sql/V3__create_address_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE address (
+    ADDRESS_STREET  VARCHAR(50),
+    ADDRESS_CITY    VARCHAR(50),
+    ADDRESS_ZIP     INT,
+    ADDRESS_COUNTRY VARCHAR(50)
+);
+
+INSERT INTO address VALUES ('1007 Mountain Drive','Gotham',53540,'USA');
+INSERT INTO address VALUES ('Disneyland Resort','Anaheim',92803,'USA');
